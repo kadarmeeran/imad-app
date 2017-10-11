14ff15eb2c63e0f20b97143ddddb619362d686fa
kadarmeeran <coolkadar92@gmail.com>
2017-10-11 10:17:58 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'kadar.html'));
 });
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
